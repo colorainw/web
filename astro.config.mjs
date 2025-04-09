4e9bfe06412a46d122a5e40ed303cde4e8faf656
JNALY <3530661536@qq.com>
2025-04-09 23:46:29 +0800

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,8 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: YukinaConfig.site,
+  site: 'https://colorainw.github.io',
+  base: '/web',
   integrations: [
     tailwind(),
     svelte(),
